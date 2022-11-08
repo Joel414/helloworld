f9dc642de07c4c625f81f1f5fb640ae343f72fd0
annsai98 <joel41444@gmail.com>
2022-11-08 01:56:41 -0500

    Manual push for Assignment 4
    Manual push of Assignment 4 due to problem pushing from VS Code

---
+++ blog-service.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+
+let posts = [];
+let categories = [];
+
+module.exports.initialize = function () {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                posts = JSON.parse(data);
+ 
+                fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        categories = JSON.parse(data);
+                        resolve();
+                    }
+                });
+            }
+        });
+    });
+}
+
+module.exports.getAllPosts = function(){
+    return new Promise((resolve,reject)=>{
+        (posts.length > 0 ) ? resolve(posts) : reject("no results returned"); 
+    });
+}
+
+module.exports.getPublishedPosts = function(){
+    return new Promise((resolve,reject)=>{
+        (posts.length > 0) ? resolve(posts.filter(post => post.published)) : reject("no results returned");
+    });
+}
+
+module.exports.getCategories = function(){
+    return new Promise((resolve,reject)=>{
+        (categories.length > 0 ) ? resolve(categories) : reject("no results returned"); 
+    });
+}
+
+module.exports.getPostsByCategory = function(category){
+    return new Promise((resolve,reject)=>{
+        let selectPosts = posts.filter(post=>post.category == category);
+
+        if(selectPosts.length == 0){
+            reject("no results returned")
+        }else{
+            resolve(selectPosts);
+        }
+    });
+}
+
+module.exports.getPostsByMinDate = function(minDateStr) {
+    return new Promise((resolve, reject) => {
+        let selectPosts = posts.filter(post => (new Date(post.postDate)) >= (new Date(minDateStr)))
+
+        if (selectPosts.length == 0) {
+            reject("no results returned")
+        } else {
+            resolve(selectPosts);
+        }
+    });
+}
+
+module.exports.getPostById = function(id){
+    return new Promise((resolve,reject)=>{
+        let findPost = posts.find(post => post.id == id);
+
+        if(findPost){
+            resolve(findPost);
+        }else{
+            reject("no result returned");
+        }
+    });
+}
+
+module.exports.addPost = function(postData){
+    return new Promise((resolve,reject)=>{
+        postData.published = postData.published ? true : false;
+        postData.id = posts.length + 1;
+        let now = new Date();
+        postData.postDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+        posts.push(postData);
+        resolve();
+    });
+}
+
+module.exports.getPublishedPostsByCategory = function(category){
+    return new Promise((resolve,reject)=>{
+        let filteredPosts = posts.filter(post => post.published && post.category == category);
+        (filteredPosts.length > 0) ? resolve(filteredPosts) : reject("no results returned");
+    });
+}
