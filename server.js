f4c56fdcf6daf307baa7e792298f0f9876390356
Joel414 <joel41444@gmail.com>
2022-11-24 08:05:02 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-/*********************************************************************************
-*  WEB322 – Assignment 04
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
-*  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name: Joel Biju Student ID: 148952203 Date: 07/11/2022
-*
-*  Cyclic Web App URL: // I have emailed regarding this
-* 
-*  GitHub Repository URL: https://github.com/Joel414/helloworld.git
-*
-********************************************************************************/
-
-
 const express = require('express');
 const blogData = require("./blog-service");
 const path = require("path");
